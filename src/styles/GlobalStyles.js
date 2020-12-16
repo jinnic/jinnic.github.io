d1e3ea6f82d9eb8b7d7a2aa913615dbd5c054f9b
jinnic <yoohyojin@gmail.com>
2020-12-16 15:45:00 -0500

    style update

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -19,11 +19,9 @@ const GlobalStyles = createGlobalStyle`
     --font-serif: 'Lora', serif;
     
 
-
     --border-radius: 5px;
     --nav-height: 60px;
     --nav-scroll-height: 70px;
-
 
     --easing: cubic-bezier(0.645, 0.045, 0.355, 1);
     --transition: all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1);
@@ -195,64 +193,6 @@ const GlobalStyles = createGlobalStyle`
     line-height: 150%;
   }
 
-  .big-heading {
-    margin: 0;
-    font-size: clamp(40px, 8vw, 80px);
-  }
-
-  .medium-heading {
-    margin: 0;
-    font-size: clamp(40px, 8vw, 60px);
-  }
-
-  .numbered-heading {
-    display: flex;
-    align-items: center;
-    position: relative;
-    margin: 10px 0 40px;
-    width: 100%;
-    font-size: clamp(26px, 5vw, var(--fz-heading));
-    white-space: nowrap;
-
-    &:before {
-      position: relative;
-      bottom: 4px;
-      counter-increment: section;
-      content: '0' counter(section) '.';
-      margin-right: 10px;
-      color: var(--green);
-      font-family: var(--font-serif);
-      font-size: clamp(var(--fz-md), 3vw, var(--fz-xl));
-      font-weight: 400;
-
-      @media (max-width: 480px) {
-        margin-bottom: -3px;
-        margin-right: 5px;
-      }
-    }
-
-    &:after {
-      content: '';
-      display: block;
-      position: relative;
-      top: -5px;
-      width: 300px;
-      height: 1px;
-      margin-left: 20px;
-      background-color: var(--lightest-navy);
-
-      @media (max-width: 1080px) {
-        width: 200px;
-      }
-      @media (max-width: 768px) {
-        width: 100%;
-      }
-      @media (max-width: 600px) {
-        margin-left: 10px;
-      }
-    }
-  }
-
   img,
   svg,
   .gatsby-image-wrapper {
@@ -319,65 +259,8 @@ const GlobalStyles = createGlobalStyle`
     &:last-of-type {
       margin: 0;
     }
-
-
-    & > code {
-      background-color: var(--light-navy);
-      color: var(--white);
-      font-size: var(--fz-sm);
-      border-radius: var(--border-radius);
-      padding: 0.3em 0.5em;
-    }
   }
 
-  ul {
-    &.fancy-list {
-      padding: 0;
-      margin: 0;
-      list-style: none;
-      font-size: var(--fz-lg);
-      li {
-        position: relative;
-        padding-left: 30px;
-        margin-bottom: 10px;
-        &:before {
-          content: '▹';
-          position: absolute;
-          left: 0;
-          color: var(--green);
-        }
-      }
-    }
-  }
-
-  blockquote {
-    border-left-color: var(--green);
-    border-left-style: solid;
-    border-left-width: 1px;
-    margin-left: 0px;
-    margin-right: 0px;
-    padding-left: 1.5rem;
-
-    p {
-      font-style: italic;
-      font-size: 24px;
-    }
-  }
-
-  hr {
-    background-color: var(--lightest-navy);
-    height: 1px;
-    border-width: 0px;
-    border-style: initial;
-    border-color: initial;
-    border-image: initial;
-    margin: 1rem;
-  }
-
-  code {
-    font-family: var(--font-serif);
-    font-size: var(--fz-md);
-  }
   .line {
     position: absolute;
     width: 17.71px;
@@ -385,57 +268,6 @@ const GlobalStyles = createGlobalStyle`
     left: 0;
     top: 0;
     background-color: var(--purple);
-  }
-  #logo {
-    color: var(--green);
-  }
-
-  .overline {
-    color: var(--green);
-    font-family: var(--font-serif);
-    font-size: var(--fz-md);
-    font-weight: 400;
-  }
-
-  .subtitle {
-    color: var(--green);
-    margin: 0 0 20px 0;
-    font-size: var(--fz-md);
-    font-family: var(--font-serif);
-    font-weight: 400;
-    line-height: 1.5;
-    @media (max-width: 1080px) {
-      font-size: var(--fz-sm);
-    }
-    @media (max-width: 768px) {
-      font-size: var(--fz-xs);
-    }
-
-    a {
-      line-height: 1.5;
-    }
-  }
-
-  .breadcrumb {
-    display: flex;
-    align-items: center;
-    margin-bottom: 50px;
-    color: var(--green);
-
-    .arrow {
-      display: block;
-      margin-right: 10px;
-      padding-top: 4px;
-    }
-
-    a {
-      font-family: var(--font-serif);
-      font-size: var(--fz-sm);
-      font-weight: 600;
-      line-height: 1.5;
-      text-transform: uppercase;
-      letter-spacing: 0.1em;
-    }
   }
 
   .gatsby-image-outer-wrapper {
