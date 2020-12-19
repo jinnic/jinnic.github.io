c146bc52e49635229c55d62b2c4e4c149169d392
jinnic <yoohyojin@gmail.com>
2020-12-18 23:24:47 -0500

    testing contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -26,6 +26,26 @@ export default function Contact() {
         <p>linkedin.com/in/hyojinyoo</p>
         <p>medium.com/@hyojin</p>
       </div>
+      <form method="post" action="https://getform.io/f/03e7aa3f-04b2-411a-a5ed-68dc9bb99d6c" id='contactForm'>
+        <label>
+          Name
+          <input type="text" name="name" id="name" rules="required|max:255"/>
+        </label>
+        <label>
+          Email
+          <input type="email" name="email" id="email" rules="required|max:255"/>
+        </label>
+        <label>
+          Subject
+          <input type="text" name="subject" id="subject" />
+        </label>
+        <label>
+          Message
+          <textarea name="message" id="message" rows="5" />
+        </label>
+        <button type="submit">Send</button>
+        <input type="reset" value="Clear" />
+      </form>
     </StyledContactSection>
   )
 }
